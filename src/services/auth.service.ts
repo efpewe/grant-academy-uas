9ff079639dc996756fd7b6dbe830bad6b995e304
Fernando Putra Wijaya <cienan20065840@gmail.com>
2026-01-23 23:51:33 +0700

    profile change

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,28 +1,27 @@
-import api from '../utils/api';
+import api from "../utils/api";
 
 export const authService = {
   async login(payload: any) {
-    const response = await api.post('/auth/login', payload);
-    return response.data; 
+    const response = await api.post("/auth/login", payload);
+    return response.data;
   },
 
   async register(payload: any) {
-    const response = await api.post('/auth/register', payload);
+    const response = await api.post("/auth/register", payload);
     return response.data;
   },
 
   async getMe() {
-    const response = await api.get('/auth/me');
+    const response = await api.get("/auth/me");
     return response.data;
   },
 
   async activation(code: string) {
-    const response = await api.post('/auth/activation', { code });
+    const response = await api.post("/auth/activation", { code });
     return response.data;
   },
 
-  async updateProfile(data: any) {
-    const response = await api.put('/auth/profile', data);
-    return response.data;
-  }
+  updateProfile(payload: FormData | any) {
+    return api.put("/users/profile", payload);
+  },
 };
